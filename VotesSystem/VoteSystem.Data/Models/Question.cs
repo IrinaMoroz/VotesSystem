8bffc07c68acb17d099bea7261a27602b2275976
IrinaMoroz <irinamvic@gmail.com>
2014-12-23 22:54:53 +0200

    init

---
+++ VotesSystem/VoteSystem.Data/Models/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoteSystem.Data
+{
+    public class Question
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int VoitCounts { get; set; }
+        public virtual Vote Vote { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
---
+++ VotesSystem/VoteSystem.Data/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoteSystem.Data
+{
+    public class User
+    {
+        public int ID { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Login { get; set; }
+
+        public virtual ICollection<Vote> Vote { get; set; }
+
+        public virtual ICollection<Question> Questions { get; set; }
+    }
+}
