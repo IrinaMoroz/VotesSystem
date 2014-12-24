4a16c48696f0c6ca070a186f2160eed0f08f71c5
IrinaMoroz <irinamvic@gmail.com>
2014-12-24 14:07:49 +0200

    init

---
+++ VotesSystem1/VoteSystem.Data/Models/Vote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoteSystem.Data
+{
+    public class Vote
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsPrivate { get; set; }
+        public string EmailRecipient { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public virtual User User { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual ICollection<Question> Questions { get; set; }
+
+    }
+}
