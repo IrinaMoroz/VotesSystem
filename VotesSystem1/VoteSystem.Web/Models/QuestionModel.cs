920104fa13b9a5f2d4a7b40e87bd8e915376c29a
IrinaMoroz <irinamvic@gmail.com>
2015-01-12 22:05:41 +0200

    fix bugs

---
+++ VotesSystem1/VoteSystem.Web/Models/QuestionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoteSystem.Data.Models
+{
+    public class QuestionModel
+    {
+        public int ID { get; set; }
+        public int VoteID{get; set;}
+        public Vote vote { get; set; }
+        public override string ToString()
+        {
+            return ID.ToString();
+        }
+    }
+}
